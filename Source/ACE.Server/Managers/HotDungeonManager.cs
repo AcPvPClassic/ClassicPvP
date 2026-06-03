e4a30d9a0eb4159f2081c7afc9243b6f3cde62c3
AcPvP <89650671+AcPvP@users.noreply.github.com>
2026-06-02 20:40:15 -0400

    Hot Dungeon starting list - needs xp and box rates set

--- Source/ACE.Server/Managers/HotDungeonManager.cs
+++ Source/ACE.Server/Managers/HotDungeonManager.cs
@@ -1,14 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using ACE.Common;
 using ACE.Common.Extensions;
 using ACE.Database;
-using ACE.Server.Network.GameMessages.Messages;
 using ACE.Entity.Enum;
-
+using ACE.Server.Network.GameMessages.Messages;
+using ACE.Server.Physics;
 using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Joins;
+using System.Text;
 
 namespace ACE.Server.Managers
 {
@@ -55,41 +56,120 @@ namespace ACE.Server.Managers
         // BoxDropChance = per-kill probability (0.0–1.0) that a monster drops A Box
         public static readonly List<HotDungeonEntry> PossibleDungeons = new List<HotDungeonEntry>
         {
-            new HotDungeonEntry { Landblock = 0x019E, MinLevel =  1, MaxLevel =  30, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Drudge Hideout
-            new HotDungeonEntry { Landblock = 0x0162, MinLevel =  1, MaxLevel =  30, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Cave of Alabree
-            new HotDungeonEntry { Landblock = 0x0153, MinLevel =  1, MaxLevel =  30, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Shreth Hive
-            new HotDungeonEntry { Landblock = 0x0156, MinLevel =  1, MaxLevel =  30, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Sea Temple Catacombs
-            new HotDungeonEntry { Landblock = 0x0163, MinLevel =  1, MaxLevel =  30, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Holtburg Redoubt
-            new HotDungeonEntry { Landblock = 0x0158, MinLevel =  1, MaxLevel =  30, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Lytaway Dungeon
-            new HotDungeonEntry { Landblock = 0x0154, MinLevel =  1, MaxLevel =  30, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Stone Cathedral
-            new HotDungeonEntry { Landblock = 0x02C6, MinLevel =  1, MaxLevel =  30, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Lightless Tunnels
-            new HotDungeonEntry { Landblock = 0x5749, MinLevel =  1, MaxLevel =  30, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Tusker Lodge
+            new HotDungeonEntry { Landblock = 0x019E, MinLevel =  1, MaxLevel =  35, XpMultiplier = 4.0f, BoxDropChance = 0.03f }, //Drudge Hideout
+            new HotDungeonEntry { Landblock = 0x0162, MinLevel =  1, MaxLevel =  35, XpMultiplier = 3.5f, BoxDropChance = 0.03f }, //Cave of Alabree
+            new HotDungeonEntry { Landblock = 0x0153, MinLevel =  1, MaxLevel =  35, XpMultiplier = 3.5f, BoxDropChance = 0.03f }, //Shreth Hive
+            new HotDungeonEntry { Landblock = 0x0156, MinLevel =  1, MaxLevel =  35, XpMultiplier = 3.5f, BoxDropChance = 0.03f }, //Sea Temple Catacombs
+            new HotDungeonEntry { Landblock = 0x0163, MinLevel =  1, MaxLevel =  35, XpMultiplier = 4.0f, BoxDropChance = 0.03f }, //Holtburg Redoubt
+            new HotDungeonEntry { Landblock = 0x0158, MinLevel =  1, MaxLevel =  35, XpMultiplier = 3.5f, BoxDropChance = 0.03f }, //Lytaway Dungeon
+            new HotDungeonEntry { Landblock = 0x0154, MinLevel =  1, MaxLevel =  35, XpMultiplier = 3.5f, BoxDropChance = 0.03f }, //Stone Cathedral
+            new HotDungeonEntry { Landblock = 0x02C6, MinLevel =  1, MaxLevel =  35, XpMultiplier = 2.5f, BoxDropChance = 0.03f }, //Lightless Tunnels
+            new HotDungeonEntry { Landblock = 0x5749, MinLevel =  1, MaxLevel =  35, XpMultiplier = 3.5f, BoxDropChance = 0.03f }, //Tusker Lodge
+            new HotDungeonEntry { Landblock = 0x01D9, MinLevel =  1, MaxLevel =  35, XpMultiplier = 4.5f, BoxDropChance = 0.03f }, //A Red Rat Lair
+            new HotDungeonEntry { Landblock = 0x01F6, MinLevel =  1, MaxLevel =  35, XpMultiplier = 3.5f, BoxDropChance = 0.03f }, //Holtburg Dungeon
 
-            new HotDungeonEntry { Landblock = 0x01D7, MinLevel =  10, MaxLevel =  40, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Sylsfear
-            new HotDungeonEntry { Landblock = 0x013B, MinLevel =  10, MaxLevel =  40, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Folthid Cellar
-            new HotDungeonEntry { Landblock = 0x01CC, MinLevel =  10, MaxLevel =  40, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Halls of the Helm
-            new HotDungeonEntry { Landblock = 0x01B4, MinLevel =  10, MaxLevel =  40, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Golem Burial Ground
-            new HotDungeonEntry { Landblock = 0x017A, MinLevel =  10, MaxLevel =  40, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Banderling Hovel
+            new HotDungeonEntry { Landblock = 0x01D7, MinLevel =  10, MaxLevel =  40, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Sylsfear
+            new HotDungeonEntry { Landblock = 0x013B, MinLevel =  10, MaxLevel =  40, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Folthid Cellar
+            new HotDungeonEntry { Landblock = 0x01CC, MinLevel =  10, MaxLevel =  40, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Halls of the Helm
+            new HotDungeonEntry { Landblock = 0x01B4, MinLevel =  10, MaxLevel =  40, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Golem Burial Ground
+            new HotDungeonEntry { Landblock = 0x017A, MinLevel =  10, MaxLevel =  40, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Banderling Hovel
+            new HotDungeonEntry { Landblock = 0x5643, MinLevel =  10, MaxLevel =  40, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Gromnie Clan Training Camp
 
-            new HotDungeonEntry { Landblock = 0x5B46, MinLevel =  12, MaxLevel =  40, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Tusker Cave
-            new HotDungeonEntry { Landblock = 0x5C47, MinLevel =  12, MaxLevel =  40, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Tusker Grotto
+            new HotDungeonEntry { Landblock = 0x5B46, MinLevel =  12, MaxLevel =  40, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Tusker Cave
+            new HotDungeonEntry { Landblock = 0x5C47, MinLevel =  12, MaxLevel =  40, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Tusker Grotto
 
-            new HotDungeonEntry { Landblock = 0x0188, MinLevel =  14, MaxLevel =  40, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Mines of Despair
+            new HotDungeonEntry { Landblock = 0x0188, MinLevel =  14, MaxLevel =  40, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Mines of Despair
 
-            new HotDungeonEntry { Landblock = 0x02F4, MinLevel =  15, MaxLevel =  50, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Halls of Metos (AB)
-            new HotDungeonEntry { Landblock = 0x01C1, MinLevel =  15, MaxLevel =  50, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Tumerok Mine
-            new HotDungeonEntry { Landblock = 0x018D, MinLevel =  15, MaxLevel =  50, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Olthoi Tunnels
-            new HotDungeonEntry { Landblock = 0x5C43, MinLevel =  15, MaxLevel =  50, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Tusker Cavern
-            new HotDungeonEntry { Landblock = 0x0140, MinLevel =  15, MaxLevel =  50, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Virindi Fort
+            new HotDungeonEntry { Landblock = 0x02F4, MinLevel =  15, MaxLevel =  60, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Halls of Metos (AB)
+            new HotDungeonEntry { Landblock = 0x01C1, MinLevel =  15, MaxLevel =  60, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Tumerok Mine
+            new HotDungeonEntry { Landblock = 0x018D, MinLevel =  15, MaxLevel =  60, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Olthoi Tunnels
+            new HotDungeonEntry { Landblock = 0x5C43, MinLevel =  15, MaxLevel =  60, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Tusker Cavern
+            new HotDungeonEntry { Landblock = 0x0140, MinLevel =  15, MaxLevel =  60, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Virindi Fort
+            new HotDungeonEntry { Landblock = 0x01A2, MinLevel =  15, MaxLevel =  60, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Swamp Temple (Yanshi)
+            new HotDungeonEntry { Landblock = 0x01AA, MinLevel =  15, MaxLevel =  60, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Lair of Death
 
-            new HotDungeonEntry { Landblock = 0x01A9, MinLevel =  18, MaxLevel =  50, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Ancient Lighthouse
+            new HotDungeonEntry { Landblock = 0x01A9, MinLevel =  18, MaxLevel =  60, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Ancient Lighthouse            
 
-            new HotDungeonEntry { Landblock = 0x02DB, MinLevel =  20, MaxLevel =  50, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Fenmalain Vestibule
-            new HotDungeonEntry { Landblock = 0x5772, MinLevel =  20, MaxLevel =  50, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Olthoi Brood Hive (20+)
+            new HotDungeonEntry { Landblock = 0x02DB, MinLevel =  20, MaxLevel =  60, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Fenmalain Vestibule
+            new HotDungeonEntry { Landblock = 0x5772, MinLevel =  20, MaxLevel =  60, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Olthoi Brood Hive (20+)
+            new HotDungeonEntry { Landblock = 0x0105, MinLevel =  20, MaxLevel =  70, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Black Spawn Den (AB)
+            new HotDungeonEntry { Landblock = 0x5E45, MinLevel =  20, MaxLevel =  70, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Black Death Catacombs
+            new HotDungeonEntry { Landblock = 0x02FD, MinLevel =  20, MaxLevel =  70, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Olthoi Horde Nest
+            new HotDungeonEntry { Landblock = 0x5D4A, MinLevel =  20, MaxLevel =  70, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Olthoi Chasm
+            new HotDungeonEntry { Landblock = 0x01A6, MinLevel =  20, MaxLevel =  70, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Disaster Maze
+            new HotDungeonEntry { Landblock = 0x0290, MinLevel =  20, MaxLevel =  70, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Stable Rift
+            new HotDungeonEntry { Landblock = 0x5C45, MinLevel =  20, MaxLevel =  70, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Tusker Abode
+            new HotDungeonEntry { Landblock = 0x01BE, MinLevel =  20, MaxLevel =  70, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Tumerok Fortress
+            new HotDungeonEntry { Landblock = 0x0138, MinLevel =  20, MaxLevel =  70, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Lost City of Frore
 
+            new HotDungeonEntry { Landblock = 0x02A9, MinLevel =  20, MaxLevel =  275, XpMultiplier = 4.0f, BoxDropChance = 0.02f }, //Hieromancers' Halls
 
-            new HotDungeonEntry { Landblock = 0x5E4A, MinLevel =  60, MaxLevel =  130, XpMultiplier = 2.5f, BoxDropChance = 0.05f }, //Olthoi Brood Hive (60+)
+            new HotDungeonEntry { Landblock = 0x03A1, MinLevel =  25, MaxLevel =  70, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Singular Pyreal Repository
+            new HotDungeonEntry { Landblock = 0x5B47, MinLevel =  25, MaxLevel =  70, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Tusker Habitat
+            new HotDungeonEntry { Landblock = 0x0109, MinLevel =  25, MaxLevel =  70, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Dungeon of Corpses
 
+            new HotDungeonEntry { Landblock = 0x5B4A, MinLevel =  30, MaxLevel =  70, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Tusker Quarters
+            new HotDungeonEntry { Landblock = 0x02FD, MinLevel =  30, MaxLevel =  70, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Olthoi Horde Nest (Bandit Castle)
+            new HotDungeonEntry { Landblock = 0x0179, MinLevel =  30, MaxLevel =  70, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Umbral Hall
+            new HotDungeonEntry { Landblock = 0x02C9, MinLevel =  30, MaxLevel =  70, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Sotiris Dungeon
+
+            new HotDungeonEntry { Landblock = 0x02A1, MinLevel =  32, MaxLevel =  70, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Panopticon
+            new HotDungeonEntry { Landblock = 0x03A0, MinLevel =  35, MaxLevel =  70, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //singular chorizite repository
+            new HotDungeonEntry { Landblock = 0x01F8, MinLevel =  35, MaxLevel =  70, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Mite Maze
+
+            new HotDungeonEntry { Landblock = 0x027C, MinLevel =  40, MaxLevel =  90, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Ancient Empyrean Grotto
+            new HotDungeonEntry { Landblock = 0x6346, MinLevel =  40, MaxLevel =  90, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Matron Hive South
+            new HotDungeonEntry { Landblock = 0x02DC, MinLevel =  40, MaxLevel =  90, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Caulnalain Vestibule
+            new HotDungeonEntry { Landblock = 0x5E4A, MinLevel =  40, MaxLevel =  90, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Olthoi Brood Hive
+            new HotDungeonEntry { Landblock = 0x5A48, MinLevel =  40, MaxLevel =  90, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Tusker Barracks
+            new HotDungeonEntry { Landblock = 0x017D, MinLevel =  40, MaxLevel =  90, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Hidden Entrance
+            new HotDungeonEntry { Landblock = 0x017B, MinLevel =  40, MaxLevel =  90, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Watery Grotto
+
+            new HotDungeonEntry { Landblock = 0x017D, MinLevel =  45, MaxLevel =  120, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Singular Obsidian Repository
+            new HotDungeonEntry { Landblock = 0x5A4A, MinLevel =  45, MaxLevel =  120, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Tusker Pits
+            new HotDungeonEntry { Landblock = 0x5D46, MinLevel =  45, MaxLevel =  120, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Vengeance Caverns
+
+            new HotDungeonEntry { Landblock = 0x5A4A, MinLevel =  50, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Tusker Armory
+            new HotDungeonEntry { Landblock = 0x0291, MinLevel =  50, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Singularity Bore
+            new HotDungeonEntry { Landblock = 0x027D, MinLevel =  50, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Lair of the Eviscerators
+            new HotDungeonEntry { Landblock = 0x5F46, MinLevel =  50, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Heart of Innocence
+
+            new HotDungeonEntry { Landblock = 0x6248, MinLevel =  60, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Renegade Fortress
+            new HotDungeonEntry { Landblock = 0x604A, MinLevel =  60, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Sclavus Temple
+            new HotDungeonEntry { Landblock = 0x02E0, MinLevel =  60, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Shendolain Vestibule
+            new HotDungeonEntry { Landblock = 0x5B48, MinLevel =  60, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Tusker Holding           
+            new HotDungeonEntry { Landblock = 0x5E4A, MinLevel =  60, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Olthoi Brood Hive (60+)
+            new HotDungeonEntry { Landblock = 0x01A3, MinLevel =  60, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //The Pit
+
+            new HotDungeonEntry { Landblock = 0x6678, MinLevel =  70, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Olthoi Warrior Nest
+            new HotDungeonEntry { Landblock = 0x5D45, MinLevel =  70, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Tusker Tunnels
+            new HotDungeonEntry { Landblock = 0x0005, MinLevel =  70, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Southern Power Forge
+
+            new HotDungeonEntry { Landblock = 0x0005, MinLevel =  75, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Plated Rifts
+
+            new HotDungeonEntry { Landblock = 0x594D, MinLevel =  80, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Ancient Temple
+            new HotDungeonEntry { Landblock = 0x01E4, MinLevel =  80, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //North Glenden Prison
+            new HotDungeonEntry { Landblock = 0x5D4D, MinLevel =  80, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Cavernous Olthoi Chasm
+            new HotDungeonEntry { Landblock = 0x5F48, MinLevel =  80, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Hidden Cavern
+            new HotDungeonEntry { Landblock = 0x5A49, MinLevel =  80, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Tusker Honeycombs
+            new HotDungeonEntry { Landblock = 0x667A, MinLevel =  80, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Mutilator Tunnels
+            new HotDungeonEntry { Landblock = 0x0010, MinLevel =  80, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //War Room
+            new HotDungeonEntry { Landblock = 0x0003, MinLevel =  80, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Niffis Fighting Pits
+
+            new HotDungeonEntry { Landblock = 0x5849, MinLevel =  85, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Tusker Assault
+
+            new HotDungeonEntry { Landblock = 0x6145, MinLevel =  90, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Matron Hive West
+            new HotDungeonEntry { Landblock = 0x6548, MinLevel =  90, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //The Orphanage
+
+            new HotDungeonEntry { Landblock = 0x0110, MinLevel =  100, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Nexus
+            new HotDungeonEntry { Landblock = 0x5949, MinLevel =  100, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Tusker Lacuna
+            new HotDungeonEntry { Landblock = 0x0017, MinLevel =  100, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Vile Sanctuary
+            new HotDungeonEntry { Landblock = 0x0023, MinLevel =  100, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Sezzherei's Lair
+
+            new HotDungeonEntry { Landblock = 0x0023, MinLevel =  120, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Matron Hive East
+
+            new HotDungeonEntry { Landblock = 0x624A, MinLevel =  60, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Burun Cavern
+            new HotDungeonEntry { Landblock = 0x6147, MinLevel =  60, MaxLevel =  275, XpMultiplier = 2.5f, BoxDropChance = 0.005f }, //Mountain Citadel
 
 
 
